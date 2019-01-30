42a6e88c37cba5bca1664c254eea6c8fa6e9d5ef
krmtrk20 <krmtrk20@gmail.com>
2019-01-30 16:17:37 +0300

    Changes on app

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -72,7 +72,7 @@ public class App
           String[] result = App.meaningfulComputation(inputIntegerArray,inputList,strArray,boolArray);
 
          Map map = new HashMap();
-          map.put("result", result.toString());
+          map.put("result", result[0]);
           return new ModelAndView(map, "compute.mustache");
         }, new MustacheTemplateEngine());
 
